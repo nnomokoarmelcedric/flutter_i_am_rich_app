d6766a8e4c9d07d60cb25b5589039f345046e995
armel cedric nnomoko <armelnnomoko237@gmail.com>
2024-05-21 00:38:41 +0100

    completed

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I am rich'),
+          backgroundColor: Colors.blue[400],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
